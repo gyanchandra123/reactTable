3289c303c1f8029ee43ca4260b30c895d1c4119b
gyanchandra moirangthem <gyanchandra moirangthem>
2021-07-11 21:31:52 +0530

    React Table Tutorial - 6 - Sorting

--- src/components/columns.js
+++ src/components/columns.js
@@ -30,3 +30,51 @@ export const COLUMNS = [
     accessor: "phone",
   },
 ];
+
+// grouping columns:
+export const GROUPED_COLUMNS = [
+    {                          // for id
+      Header: 'Id',
+      Footer: 'Id',
+      accessor: 'id'
+    },
+
+    {                        // for name group
+      Header: 'Name',
+      Footer: 'Name',
+      columns: [
+        {
+          Header: 'First Name',
+          Footer: 'First Name',
+          accessor: 'first_name'
+        },
+        {
+          Header: 'Last Name',
+          Footer: 'Last Name',
+          accessor: 'last_name'
+        }
+      ]
+    },
+
+    {                       // for info group.
+      Header: 'Info',
+      Footer: 'Info',
+      columns: [
+        {
+          Header: 'Date of Birth',
+          Footer: 'Date of Birth',
+          accessor: 'date_of_birth'
+        },
+        {
+          Header: 'Country',
+          Footer: 'Country',
+          accessor: 'country'
+        },
+        {
+          Header: 'Phone',
+          Footer: 'Phone',
+          accessor: 'phone'
+        }
+      ]
+    }
+  ]
